6eaa80ff8094841b6c6847abdc3f899e4593cc95
Misaelsharkbot <101156132+Misaelsharkbot@users.noreply.github.com>
2022-04-12 11:36:56 -0500

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -3,11 +3,16 @@ let info = `
 *Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de AGROMOS SP - Bot*
 
 *➤ Grupo oficial del Bot:*
+*1.-* https://chat.whatsapp.com/E9qJfvlLjENKAxnhII9rao
+
+*2.-* https://chat.whatsapp.com/JSW9rrLeL5D5aVr7j023Pz
+
+*➤ Grupo oficial del desarollador del Bot:*
 *1.-https://chat.whatsapp.com/CBoAGmEFcCy44xIoWBSgzJ
 
-*➤ Grupo2 oficial del Bot:*
+
 *2.-https://chat.whatsapp.com/FZsoX7iGBZLBQgFX9MUXIy
-*➤ Grupo3 oficial del Bot:*
+
 *3.-https://chat.whatsapp.com/K0UCo7igED92q9c1w2DiEH
 `.trim() 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 AGROMOS SP - BOT 🔥*', 'status@broadcast')}
